9e76f1d45f76472d3f4de88c9739ed1d462f61f7
Neeraj Kushwaha <109076839+Neerajkush22@users.noreply.github.com>
2022-08-01 16:24:56 +0530

    WeekendTask_CrudOperation

---
+++ WeekendTask/src/main/java/com/bookself/book/Models/Book.java
@@ -0,0 +1,18 @@
+package com.bookself.book.Models;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Table(name = "BookDetails")
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    @Column
+    private String bookName;
+    @Column
+    private String authorName;
+}
