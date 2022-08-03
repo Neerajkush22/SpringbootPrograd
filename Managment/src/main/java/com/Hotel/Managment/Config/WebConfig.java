914dab89fbce5a78fd96a85c27d990b79b649ac5
Neeraj Kushwaha <109076839+Neerajkush22@users.noreply.github.com>
2022-08-03 22:17:43 +0530

    Add Spring Security

--- Managment/src/main/java/com/Hotel/Managment/Config/WebConfig.java
+++ Managment/src/main/java/com/Hotel/Managment/Config/WebConfig.java
@@ -1,9 +1,11 @@
 package com.Hotel.Managment.Config;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
 public class WebConfig implements WebMvcConfigurer {
     public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
         configurer.defaultContentType(MediaType.APPLICATION_XML);
