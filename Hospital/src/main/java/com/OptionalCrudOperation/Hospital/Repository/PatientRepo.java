fbf33f7dc5ddeb5d11584455f99a7b2adec491fa
Neeraj Kushwaha <109076839+Neerajkush22@users.noreply.github.com>
2022-08-08 15:18:49 +0530

    Delete Hospital directory

--- Hospital/src/main/java/com/OptionalCrudOperation/Hospital/Repository/PatientRepo.java
+++
@@ -1,7 +0,0 @@
-package com.OptionalCrudOperation.Hospital.Repository;
-
-import com.OptionalCrudOperation.Hospital.Model.Hospital;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PatientRepo extends JpaRepository<Hospital,Integer> {
-}
