0222d1ddaaa3922c6daee6dd4a1a4ffa39603d72
Neeraj Kushwaha <109076839+Neerajkush22@users.noreply.github.com>
2022-08-09 20:56:03 +0530

    Delete Managment directory

--- Managment/src/main/java/com/Hotel/Managment/Repository/CustomerRepo.java
+++
@@ -1,7 +0,0 @@
-package com.Hotel.Managment.Repository;
-
-import com.Hotel.Managment.Model.Hotel;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CustomerRepo extends JpaRepository<Hotel,Integer> {
-}
