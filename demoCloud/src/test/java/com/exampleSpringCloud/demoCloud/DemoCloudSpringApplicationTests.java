2094f2516198807076741483a2e647dcc5887a47
Neeraj Kushwaha <109076839+Neerajkush22@users.noreply.github.com>
2022-08-16 23:18:54 +0530

    [ADD] SpringCloudDependency

---
+++ demoCloud/src/test/java/com/exampleSpringCloud/demoCloud/DemoCloudSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exampleSpringCloud.demoCloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoCloudSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
